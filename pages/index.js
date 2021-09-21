ba5bbf4e250f8c7deddbab78242e2278ce3fa6f7
mapsam <matthews.sam@gmail.com>
2021-09-20 22:39:40 -0700

    show env

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,21 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 export async function getStaticProps() {
-  const welcome = process.env.WELCOME || 'Uh oh';
-  const env = process.env.ENV || 'Unknown';
-  return { props: { welcome, env } };
+  const name = process.env.NAME || 'Unknown';
+  const env = Object.keys(process.env).map((e) => {
+    return {key: e, val: process.env[e] }
+  });
+
+  return { props: { env, name } };
 }
 
-export default function Index({ welcome, env }) {
+export default function Index({ env, name }) {
+  const vars = env
+    .sort((a, b) => a.key > b.key)
+    .map((e, i) => {
+      return (<li key={i}><strong>{e.key}</strong>: {e.val}</li>);
+    });
+
   return (
     <div>
       <Head>
@@ -15,8 +24,15 @@ export default function Index({ welcome, env }) {
         <meta name='description' content='envercel does next.js and vercel environments' />
       </Head>
 
-      <p>{welcome}! This is the <strong>{env}</strong> environment.</p>
+      <p>This is a Next.js app. Using the <strong>{name}</strong> configuration.</p>
       <p>See more at <Link href='https://github.com/mapsam/envercel'>github.com/mapsam/envercel</Link>.</p>
+
+      {env.VERCEL &&
+        <p>This app is running on Vercel in the <strong>{env.VERCEL_ENV}</strong> environment.</p>
+      }
+
+      <p>Environment:</p>
+      <ul>{vars}</ul>
     </div>
   )
 }
